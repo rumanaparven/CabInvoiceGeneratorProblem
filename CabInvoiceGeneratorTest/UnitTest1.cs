0f65fad58ce5d5c7e3ea63774ec0b618d2678f30
Rumana <rumanaparven16@gmail.com>
2020-10-29 22:20:28 +0530

    [Rumana] Add. Calculate Fare for multiple entries.

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -17,5 +17,16 @@ namespace CabInvoiceGeneratorTest
             double expected = 25;
             Assert.AreEqual(expected, fare);
         }
+
+        [Test]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            Assert.AreEqual(expectedSummary, summary);
+        }
     }
 }
