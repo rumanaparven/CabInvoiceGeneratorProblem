44e73b8cfa94b5753c4518172c4921818d357eab
Rumana <rumanaparven16@gmail.com>
2020-10-29 22:14:41 +0530

    [Rumana] Add. Given Distance And Time Should Return Total Fare

---
+++ CabInvoiceGeneratorProblem/RideRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGeneratorProblem
+{
+    public class RideRepository
+    {
+        Dictionary<string, List<Ride>> userRides = null;
+
+        public RideRepository()
+        {
+            this.userRides = new Dictionary<string, List<Ride>>();
+        }
+
+
+        public void AddRide(string userID,Ride[] rides)
+        {
+            bool rideList = this.userRides.ContainsKey(userID);
+            try
+            {
+                if (!rideList)
+                {
+                    List<Ride> list = new List<Ride>();
+                    list.AddRange(rides);
+                    this.userRides.Add(userID, list);
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "Rides are null");
+            }
+        }
+
+        public Ride[] GetRides(string userID)
+        {
+            try
+            {
+                return this.userRides[userID].ToArray();
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid userID");
+            }
+        }
+    }
+}
---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using CabInvoiceGeneratorProblem;
+using NUnit.Framework;
+
+namespace CabInvoiceGeneratorTest
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
